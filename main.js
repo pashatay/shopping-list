1521d8c24f1d990f04f953319138e47b38463f9f
Pasha Tay <pashabonilla@gmail.com>
2019-08-26 13:11:28 -0400

    edit js file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $('#addBttn').on('click', function(e){
     e.preventDefault();
     let text = $('input').val();
+    if(text.length>0){
     let addLi = `<li><span class="shopping-item">${text}</span>
     <div class="shopping-item-controls">
       <button class="shopping-item-toggle">
@@ -11,10 +12,19 @@ $('#addBttn').on('click', function(e){
       </button>
     </div>
   </li>`;
-    $('.shopping-list').append(addLi)
+    $('.shopping-list').append(addLi);
+    text = $('input').val('');
+    } else{
+        alert('Add the note first!')
+    }
 })
 
-$('ul').on('click', '.shopping-item-toggle', function(e){
-    event.stopPropagation();
-    $(this).find('.shopping-item').fadeOut();
+$('ul').on('click', '.shopping-item-toggle', function(){
+    $(this).closest("li").find(".shopping-item").toggleClass('shopping-item__checked');
+})
+
+$('ul').on('click', '.shopping-item-delete', function(){
+    $(this).closest("li").fadeOut(400, function(){
+        $(this).remove();
+    });
 })
